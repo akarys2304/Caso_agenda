b1baa83ed7cb0ffeeb3cd365f98deb1b7dbd62c8
Aline Manhães <142928899+alinemmmanhaes@users.noreply.github.com>
2026-05-21 10:24:18 -0300

    Delete agenda_telefonica directory

--- agenda_telefonica/src/app/componentes/cadastrar/cadastrar.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-cadastrar',
-  standalone: false,
-  templateUrl: './cadastrar.html',
-  styleUrl: './cadastrar.scss',
-})
-export class Cadastrar {}
--- agenda_telefonica/src/app/componentes/editar/editar.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-editar',
-  standalone: false,
-  templateUrl: './editar.html',
-  styleUrl: './editar.scss',
-})
-export class Editar {}
