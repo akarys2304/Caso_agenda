e333cfc0f3a2b87fb107d133cdb1d241236e46c0
Karys Barbosa <karys.barbosa@elogroup.com.br>
2026-05-21 10:16:08 -0300

    nome alterado

---
+++ agenda_telefonica_front/src/app/componentes/cadastrar/cadastrar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastrar',
+  standalone: false,
+  templateUrl: './cadastrar.html',
+  styleUrl: './cadastrar.scss',
+})
+export class Cadastrar {}
---
+++ agenda_telefonica_front/src/app/componentes/contatos/contatos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contatos',
+  standalone: false,
+  templateUrl: './contatos.html',
+  styleUrl: './contatos.scss',
+})
+export class Contatos {}
